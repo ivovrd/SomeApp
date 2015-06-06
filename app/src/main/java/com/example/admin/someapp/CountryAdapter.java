097b2314904fe4dbd75729fe10745e62f0faebb2
ivovrd <ivo.vrdoljak@gmail.com>
2015-06-06 17:30:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/someapp/CountryAdapter.java
@@ -0,0 +1,58 @@
+package com.example.admin.someapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.admin.someapp.model.Country;
+
+import java.util.List;
+
+/**
+ * Created by ADMIN on 5.6.2015..
+ */
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHolder>{
+
+    private List<Country> countries;
+    private int rowLayout;
+    private Context mContext;
+
+    public CountryAdapter(List<Country> countries, int rowLayout, Context mContext){
+        this.countries = countries;
+        this.rowLayout = rowLayout;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Country country = countries.get(position);
+        holder.countryName.setText(country.name);
+        holder.countryImage.setImageDrawable(mContext.getDrawable(country.getImageResourceId(mContext)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries == null ? 0 : countries.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView countryName;
+        public ImageView countryImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            countryName = (TextView)itemView.findViewById(R.id.countryName);
+            countryImage = (ImageView)itemView.findViewById(R.id.countryImage);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/admin/someapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.admin.someapp;
+
+import android.app.Activity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.admin.someapp.model.CountryManager;
+
+
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mAdapter = new CountryAdapter(CountryManager.getInstance().getCountries(), R.layout.item_view, this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
